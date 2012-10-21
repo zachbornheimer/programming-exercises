7f3848669f19cf1c4e778f7884ab48a650f3efec
Z. Bornheimer <z@chary.me>
2012-10-21 19:48:03 -0400

    Addd tail-recursive max called maxr.  Also added .gitignore.
    maxr takes any input and processes it looking for the largest number using tail recursion.

--- lisp/lisp.lisp
+++ lisp/lisp.lisp
@@ -2,7 +2,7 @@
   "These solutions are for the book __Lisp__ by Winston and Horn, 3rd edition.  These solutions were written by Z. Bornheimer.  They are relased as is (warantee-less).")
 (defvar *chapters* '(3 4))
 (defvar *chapter3* '((1 exchange) (2 construct) (3 rotate-left) (4 rotate-right) (5 palindromize) (6 (f-to-c c-to-f))))
-(defvar *chapter4* '((1 divisible-by-three) (2 palidnromep) (3 rightp)))
+(defvar *chapter4* '((1 divisible-by-three) (2 palidnromep) (3 rightp) (5 abs-min-max) ))
 
 (defun exchange (var)
   (list (first (last var)) (first var)))
@@ -33,3 +33,54 @@
 
 (defun rightp (hyp side1 side2)
   (zerop (- (* hyp hyp) (+ (* side1 side1) (* side2 side2)))))
+
+(defun abs-min-max (function-name arg1 &optional arg2)
+  (when (equal function-name 'ABS)
+    (absr arg1))
+  (when (equal function-name 'MAX)
+    (maxr arg1 arg2))
+)
+
+(defun absr (l)
+  ;; This function is a wrapper for tail-recurse-abs
+  ;; absr extends the functionality of abs using tail
+  ;; recursion and type predicate identification.
+  (tail-recurse-abs '() l))
+
+(defun tail-recurse-abs (progress toabs)
+  (if (and (listp toabs) (not (null toabs)))
+    (let ((ftoabs (first toabs)))
+      (when (and (numberp ftoabs) (< ftoabs 0))
+        (setf ftoabs (* ftoabs -1)))
+      (tail-recurse-abs (append progress (cons ftoabs '())) (rest toabs)))
+  (and
+    (when (atom toabs)
+      (and
+        (when (< toabs 0)
+          (setf toabs (* toabs -1)))
+        (setf progress toabs)))   
+    progress)))
+
+(defun maxr (&rest l)
+  (if (null l) 
+    'nil
+    (tail-recurse-max 'nil l '())))
+
+(defun tail-recurse-max (largest firstelement restelement)
+  ; Atomic?  Do largest.
+  (if (and (numberp firstelement) (not (null firstelement)))
+    (and (if (null largest)
+        (setf largest firstelement)
+        (if (> firstelement largest)
+          (setf largest firstelement)
+          largest))
+      ; Is there more to look through?  Do that.
+      (if (not (null restelement))
+        (tail-recurse-max largest (append restelement '()) '())
+        largest))
+    ; Non-Null and not an atom?  Must be a list:
+    (if (and (not (atom firstelement)) (not (null firstelement)))
+      ; First of the firstelement.  Cons Rest to the rest.  Recurse.
+      (tail-recurse-max largest (first firstelement) (append (rest firstelement) restelement))
+      (when (and (atom firstelement) (not (null firstelement)))
+        (tail-recurse-max largest restelement '())))))
