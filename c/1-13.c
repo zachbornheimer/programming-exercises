5e721ccdd96167bf87c2d96e3013c96a025d78ae
Z. Bornheimer <z@chary.me>
2012-11-17 12:32:17 -0500

    Completed Exercise 1-13 (only horizontal, did not do vertical)

---
+++ c/1-13.c
@@ -0,0 +1,32 @@
+/*
+Exercise 1-13 in K&R 2nd Edition
+Horizontal Histuogram
+Written by Z. Bornheimer (provided as is without warranty).
+*/
+#include <stdio.h>
+
+#define MAXLENGTH 1000
+
+/* print a horizontal histogram of word lengths from input. */
+main()
+{
+    int c, w;
+    int wl[MAXLENGTH];
+    w = 0; /* number of words processed */
+    wl[w] = 0; /* initialize the first element length */
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\n' || c == '\t') {
+            ++w;
+            wl[w] = 0;
+        } else {
+            ++wl[w];
+        }
+    }
+    int i, j;
+    for (i = 0; i <= w; ++i) {
+        for (j = 0; j < wl[i]; ++j)
+            printf("=");
+        printf("\n");
+    }
+}
